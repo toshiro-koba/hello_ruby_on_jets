78a12bbdb0b07f090653d2f8afc4c9088d086eb5
toshiro-koba <kobato.231.0625@gmail.com>
2020-12-02 09:34:37 +0900

    コントローラーにアクションを定義

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,5 +1,37 @@
 class GreetingsController < ApplicationController
   def index
-    render plain: 'hello from ruby on jets!!'
+    @random = "https://source.unsplash.com/random"
+    @photos = random
+  end
+
+  # ９０枚の検索結果を取得
+  def search
+    photos = []
+    3.times do |i|
+      search_results = Unsplash::Photo.search("cats", page = (i+1), per_page = 30)
+      search_results.each do |photo|
+        photos << photo
+      end
+    end
+    return photos
+  end
+
+  def random
+    photos = []
+    3.times do |i|
+      search_results = Unsplash::Photo.random(count: 30)
+      search_results.each do |photo|
+        photos << photo
+      end
+    end
+    return photos
+    
+  end
+
+  Unsplash.configure do |config|
+    config.application_access_key = ENV["UNSPLASH_APPLICATION_ACCESS_KEY"]
+    config.application_secret = ENV["UNSPLASH_APPLICATION_SECRET"]
+    config.application_redirect_uri = "https://your-application.com/oauth/callback"
+    config.utm_source = "image-falling-app"
   end
 end
